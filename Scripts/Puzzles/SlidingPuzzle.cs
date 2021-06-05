bee54c52217391f4ca3918f9e80b060333df0865
Razstvien <70968794+Razstvien@users.noreply.github.com>
2021-06-05 22:26:03 +0800

    Add files via upload
    - Sliding Puzzle
    * 1 click to move is now allowed
    * Added difficulty
    * improved algo
    * Resize all the picture assets

--- Scripts/Puzzles/SlidingPuzzle.cs
+++ Scripts/Puzzles/SlidingPuzzle.cs
@@ -13,13 +13,8 @@ public class SlidingPuzzle : Puzzle{
 		timer = GetNode<Timer>("Timer");
 		whole = GetNode<TextureRect>("Whole");
 		//* Sets as 1, 1 to avoid conflict
-		prevMove = new int[] { 1, 1 };
+		pPos = new int[] { 1, 1 };
 		
-		GD.Print(path += rnd.Next(1, 5).ToString() + "/");
-		
-		whole.Texture = UF.getTexture(ProjectSettings.GlobalizePath(path + "whole.jpg"));
-		
-		init("", 8);
 	}
 	
 	
@@ -27,8 +22,28 @@ public class SlidingPuzzle : Puzzle{
 	public override void initDifficulty(int level){
 		base.initDifficulty(level);
 		
-		row = 3;
-		col = 3;
+		if(level > 2 && level < 5){
+			path += "Normal/0";
+			
+			row = 3;
+			col = 3;
+		} else if(level > 4 && level < 7){
+			path += "Hard/0";
+			row = 3;
+			container.Columns = col = 4;
+			container.RectPosition =  new Vector2(80, 35);
+		} else if(level > 6 && level < 10){
+			path += "Expert/0";
+			
+			row = 3;
+			container.Columns = col = 5;
+			container.RectPosition = new Vector2(35, 35);
+			whole.RectPosition = new Vector2(440, 104);
+		} 
+		
+		path += rnd.Next(1, 6) + "/";
+		whole.Texture = UF.getTexture(ProjectSettings.GlobalizePath(path + "/whole.jpg"));
+		
 		tiles = new TextureButton[col, row];
 		
 		for(int i = 0; i < row; ++i){
@@ -36,7 +51,25 @@ public class SlidingPuzzle : Puzzle{
 				container.AddChild(tiles[j, i] = getTile(i, j));
 		}
 		
-		timer.Start();
+	}
+	
+	private void onDelayTimeout()
+		=> timer.Start();
+	
+	private TextureButton checkNoTile(params int[] x){
+		//*	LEFT
+		if(canMove(x[1] - 1, x[0]) && tiles[x[1] - 1, x[0]].EditorDescription.Equals("Way Labot"))
+			return tiles[x[1] - 1, x[0]];
+		//*	TOP
+		if(canMove(x[1], x[0] - 1) && tiles[x[1], x[0] - 1].EditorDescription.Equals("Way Labot"))
+			return tiles[x[1], x[0] - 1];
+		//*	RIGHT
+		if(canMove(x[1] + 1, x[0]) && tiles[x[1] + 1, x[0]].EditorDescription.Equals("Way Labot"))
+			return tiles[x[1] + 1, x[0]];
+		//*	BOTTOM
+		if(canMove(x[1], x[0] + 1) && tiles[x[1], x[0] + 1].EditorDescription.Equals("Way Labot"))
+			return tiles[x[1], x[0] + 1];
+		return null;
 	}
 	
 //***********************************************************************
@@ -44,39 +77,25 @@ public class SlidingPuzzle : Puzzle{
 	//* Select, Unselect or move a tile
 	private void onPressed(int i, int j){
 		TextureButton tile1 = tiles[j, i];
-		TextureButton tile2 = (pPos != null) ? tiles[pPos[1], pPos[0]] : null;
-		string str1 = tile1.EditorDescription;
-		string str2 = (tile2 != null) ? tile2.EditorDescription : null;
+		TextureButton tile2 = null;
 		
-		//* If the tile is being pressed once.
-		// if(tile1 != null && tile2 == null || tile1 == tile2){
-		if(tile2 == null || tile1 == tile2){
-			//* The dark tile has only "Way Labot" in the EditorDescription
-			if(!str1.Equals("Way Labot"))
-				swapTextureAfterPressed(tile1);
-				
-		//* If the tile is being pressed twice
-		// } else if(tile1 != null && tile2 != null){
-		} else if(tile2 != null){
-			if(!str1.Equals("Way Labot") && !str2.Equals("Way Labot")){
-				swapTextureAfterPressed(tile1);
-				swapTextureAfterPressed(tile2);
-				pPos = null;
-			} else if(str1.Equals("Way Labot")){
-				swapTextureAfterPressed(tile2);
-				
-				//* If the dark tile is pressed in the second click, move the tile
-				if(canMove(i, j)) moveATile(tile1, tile2);
-			} 
-		} pPos = (pPos == null && tile1 != tile2) ? new int[] { i, j } : null;
+		if((tile2 = checkNoTile(i, j)) == null)
+			return;
 		
-		GD.Print("Solved? ", checkWin());
+		moveATile(tile1, tile2);;
 		
-		//* Solved if the puzzle is being solved
+		// //* Solved if the puzzle is being solved
 		if(checkWin()){
 		//     solved();
-			
+			GD.Print("WIN");
 		}
+	}
+	
+	private bool isAllVisited(){
+		foreach(TextureButton b in tiles){
+			if(b.EditorDescription.Contains("Tile "))
+				return false;
+		} return true;
 	}
 	
 	//* Signal method from the timer's method onTimeOut(), auto disarrange the tile before the game starts
@@ -88,21 +107,26 @@ public class SlidingPuzzle : Puzzle{
 					int temp, k, l;
 					do{
 						temp = rnd.Next(1, 5);
+							
 						k = i; l = j;
-						if(temp == 1){  //* Move to left
+						if(temp == 1){  //* Move to right
+							if(isAllVisited())
+								continue;
 							if(j + 1 < col)
 								l += 1;
-						} else if(temp == 2){   //* Move to top
+						} else if(temp == 2){   //* Move to bottom
+							if(isAllVisited())
+								continue;
 							if(i + 1 < row)
 								k += 1;
-						} else if(temp == 3){   //* Move to right
+						} else if(temp == 3){   //* Move to left
 							if(j - 1 > -1)
 								l -= 1;
-						} else if(temp == 4){   //* Move to bottom
+						} else if(temp == 4){   //* Move to top
 							if(i - 1 > -1)
 								k -= 1;
 						}   //* Loop back is the k and l index are the same as the previous or the current one
-					} while(isLastPos(prevMove, k, l) || (i == k && j == l));
+					} while(isLastPos(pPos, k, l) || (i == k && j == l));
 					
 					//* Remove the word "Tile " in the tile's name, in order to know if the tile's position is already visited by the dark tile
 					if(tiles[l, k].EditorDescription.Contains("Tile "))
@@ -110,18 +134,18 @@ public class SlidingPuzzle : Puzzle{
 					
 					//* Pressed twice
 					onPressed(k, l);
-					onPressed(i, j);
 					
 					//* Assigned the current pos as the previous move
-					prevMove = new int[] { i, j };
+					pPos = new int[] { i, j };
 					
 					//* Break out the double loop
 					done = true;
 				}
 			}
-		} if(!doRepeat())   //* If done repeat thn the timer stops
+		}  if(!doRepeat() && !checkWin())   //* If done repeat thn the timer stops
 			timer.Stop();
 	}
+	
 	
 	//* Returns a tile
 	private TextureButton getTile(int i, int j){
@@ -133,6 +157,7 @@ public class SlidingPuzzle : Puzzle{
 		tile.RectSize = tile.RectMinSize = new Vector2(75, 75);
 		tile.EditorDescription = (i != 0 || j !=0 ) ?  $"Tile [{j}, {i}]" : "Way Labot";
 		tile.Connect("pressed", this, nameof(onPressed), new GA { i, j });
+		
 		return tile;
 	}
 	
@@ -162,11 +187,14 @@ public class SlidingPuzzle : Puzzle{
 	
 	//* Move a tile, 2 tiles just exchanged some information
 	private void moveATile(TextureButton tileA, TextureButton tileB){
-		string name = tileA.Name, eD = tileA.EditorDescription;
-		Texture img = tileA.TextureNormal;
+		string name = tileB.Name, eD = tileB.EditorDescription;
+		Texture img = tileB.TextureNormal;
 		
-		swapInfo(tileA, tileB.EditorDescription, tileB.TextureNormal, tileB.TexturePressed);
-		swapInfo(tileB, eD, img, null);
+		
+		swapInfo(tileB, tileA.EditorDescription, tileA.TextureNormal, tileA.TexturePressed);
+		swapInfo(tileA, eD, img, null);
+		
+		// swapTextureAfterPressed(tileB);
 	}
 	
 	//* Swap some information between 2 tiles
@@ -176,30 +204,16 @@ public class SlidingPuzzle : Puzzle{
 		tile.TexturePressed = tile.TextureHover = pressed;
 	}
 	
-	//* Swap texture after being pressed in order to know if the tile is being selected
-	private void swapTextureAfterPressed(TextureButton tileA){
-		Texture img = tileA.TextureNormal;
-		tileA.TextureNormal = tileA.TexturePressed;
-		tileA.TexturePressed = img;
-	}
-	
 	//* Checks if the current indices is the same as the previous indices
-	private bool isLastPos(int[] last, int curX, int curY) => (last[0] == curX && last[1] == curY);
+	private bool isLastPos(int[] last, int curX, int curY) => (!isAllVisited() && last[0] == curX && last[1] == curY);
 	
-	//* Checks if the tile plan to move is valid or not
-	private bool canMove(int i, int j) => canMoveLeft(i, j) || canMoveUp(i, j) || canMoveRight(i, j) || canMoveDown(i, j);
-	
-	//* Checks if it is valid to move the tile to left
-	private bool canMoveLeft(int i, int j) => ((pPos[1] - 1 == j) && pPos[0] == i) ? true : false;
-	
-	//* Checks if it is valid to move the tile to up
-	private bool canMoveUp(int i, int j) => (pPos[1] == j && (pPos[0] - 1 == i)) ? true : false;
-	
-	//* Checks if it is valid to move the tile to right
-	private bool canMoveRight(int i, int j) => ((pPos[1] + 1 == j) && pPos[0] == i) ? true : false;
-	
-	//* Checks if it is valid to move the tile to down
-	private bool canMoveDown(int i, int j) => (pPos[1] == j && (pPos[0] + 1 == i)) ? true : false;
+	private bool canMove(int x, int y){
+		try{
+			if(tiles[x, y].EditorDescription.Equals("Way Labot"))
+				return true;
+		}catch(IndexOutOfRangeException){} 
+		return false;
+	}
 	
 //***********************************************************************8
 //* Fields
@@ -208,7 +222,7 @@ public class SlidingPuzzle : Puzzle{
 	private Timer timer;
 	private TextureRect whole;
 	private int row, col;
-	private int[] pPos, prevMove;
+	private int[] pPos;
 	private Random rnd = new Random();
-	private string path = "res://Assets/Puzzles/SlidingPuzzlePic/0";
+	private string path = "res://Assets/Puzzles/SlidingPuzzlePic/";
 }
