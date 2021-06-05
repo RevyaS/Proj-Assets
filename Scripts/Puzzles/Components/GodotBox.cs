5364d6d0fb979e4d3d35a01fa1fcc5b432bf4533
Razstvien <70968794+Razstvien@users.noreply.github.com>
2021-06-05 22:20:50 +0800

    Add files via upload
    In exchange of Player.cs

---
+++ Scripts/Puzzles/Components/GodotBox.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class GodotBox : KinematicBody2D{
+	public override void _Ready()
+		=> Visible = true;
+	
+    
+	public override void _PhysicsProcess(float delta)
+		=> MoveAndSlide(speed * direction() * delta);
+	
+    //* To know which direction of mouse cursor go
+	private Vector2 direction()
+		=> (GetGlobalMousePosition() -  GlobalPosition).Floor();
+    
+    
+	private int speed = 250;
+	
+}
